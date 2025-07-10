d4022b8890b8426aedd4479b2cddb2181808a1c4
Sudharshan H G <sudharshanhg2003@gmail.com>
2025-07-10 09:28:56 +0000

    remove years 2018, 2019, 2020 and 2021 in the gallery dropdown section i

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -19,10 +19,6 @@ const galleryData = {
   '2024': ['1.jpg', '2.jpg', '3.jpg', '4.1.jpg'],
   '2023': ['1.jpg', '2.jpg', '3.jpg'],
   '2022': ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
-  '2021': Array(5).fill(0),
-  '2020': Array(7).fill(0),
-  '2019': Array(4).fill(0),
-  '2018': Array(6).fill(0),
 };
 
 const years = Object.keys(galleryData).sort((a, b) => Number(b) - Number(a));
